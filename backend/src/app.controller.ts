28cee8f6fd50680293efc7e00c228a3704cdfc17
Jableed43 <javiernehuenlopez@gmail.com>
2023-08-18 21:59:50 -0300

    agregados verbos put y delete

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Res, Post, Delete, Put, Body } from '@nestjs/common';
+import { Controller, Get, Res, Post, Delete, Put, Body, Param } from '@nestjs/common';
 import { Response } from 'express';
 import {SongsService} from './songs/songs.service'
 
@@ -9,22 +9,61 @@ export class SongsController {
 
   @Get()
   getAll(@Res() res: Response ){
-   res.send({data: this.songsService.getAll(), status: true, code:200})
+   res.send({data: this.songsService.getAll(), success: true, code:200})
   }
 
   @Post()
   create(@Body() song: any){
-   this.songsService.create(song)
-   return {message: 'Data saved', song: song, status: true, code: 201}
+   const newSong = this.songsService.create(song)
+   return {
+    message: 'Data saved',
+    song: song,
+    success: true,
+    code: 201,
+    data: newSong
+  }
+  }
+  
+  @Delete(':id')
+  deleteSongById(@Param('id') id: string){
+    const deletedSuccess = this.songsService.deleteSongById(id);
+    if(deletedSuccess){
+      return {
+        message: 'Song deleted',
+        success: true,
+        code: 200
+      } 
+    } else {
+        return {
+          message: 'Error',
+          success: false,
+          code: 400
+      }
+    }
   }
 
-  @Delete()
-  deleteSongs(){
-   return 'registro eliminado'
-  }
-
-  @Put()
-  putSongs(){
-   return 'registro modificado'
+  //path param
+  @Put(':id')
+  updateSongById(@Param('id') id: string, @Body() body: any){
+    try {
+      const parsedID = parseInt(id, 10);
+      const editSuccess = this.songsService.updateSongById(parsedID, body);
+    if(editSuccess){
+      return {
+        message: 'Song edited',
+        success: true,
+        code: 200,
+        data: editSuccess
+      } 
+    } else {
+        return {
+          message: 'Error',
+          success: false,
+          code: 400
+      }
+    }
+    } catch (error) {
+      throw new Error(`Update failed`)
+    }
   }
 }
--- backend/src/songs/songs.service.ts
+++ backend/src/songs/songs.service.ts
@@ -19,10 +19,48 @@ export class SongsService {
             const data = readParse();
             data.push(newPista);
             fs.writeFileSync(this.songs, JSON.stringify(data, null, 2))
+            return data
         } catch (error) {
-            console.log(error);
+            throw new Error("Created failed");
         }
     }
 
+    deleteSongById(id: string){
+try {
+    //traer los datos
+    const data = readParse();
+    //buscar ubicacion del dato a borrar
+    const songFound = data.findIndex((song: { id: number; }) => song.id === Number(id))
+    //verificar si encontró el song
+    if(songFound >= 0){
+        //splice(starter, deleteCount) - borra el registro
+        data.splice(songFound, 1)
+        //guardar el array de songs en el archivo json
+        fs.writeFileSync(this.songs, JSON.stringify(data, null, 2))
+        return true;
+    } else {
+        return false;
+    }
+} catch (error) {
+    throw new Error(`Delete Failed`)
+}
+    }
 
+    updateSongById(id: number, body: any){
+try {
+    const data = readParse();
+    const index = data.findIndex((song : {id : number}) => song.id === id )
+    if(index >= 0){
+        //sobreescribimos en el registro a editar la data que nos llega
+        //si se repiten campos el ultimo escrito es el que prevalece
+        const editedSong = {...data[index], ...body, id}
+        //guardamos en el registro lo editado
+        data[index] = editedSong
+        fs.writeFileSync(this.songs, JSON.stringify(data, null, 2))
+        return editedSong;
+    } return null;
+} catch (error) {
+    throw new Error(`Update Failed`)
+}
+    }
 }
